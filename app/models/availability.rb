ec7ed6bd5c9ce6f9a504e807ecba93fc70f61f82
David Carmo <dmcarmo@gmail.com>
2024-09-19 11:01:30 +0100

    Fixes issue with creating an availability on a single day. Changes seed to be executed only in development

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -7,6 +7,8 @@ class Availability < ApplicationRecord
   private
 
   def end_time_after_start_time
-    errors.add(:end_time, "must be after the start time") if end_time <= start_time
+    errors.add(:end_time, "must be after the start time") if end_time < start_time
+    # TODO: - if we change to use time, end_time should be <= start_time,
+    # for now it's < because we're only checking the date
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,10 @@
-User.destroy_all
+if Rails.env.development?
+  User.destroy_all
 
-user_1 = User.create(name: "john", email: "john@email.com", password: "12345678")
-user_2 = User.create(name: "martin", email: "martin@email.com", password: "12345678")
-user_3 = User.create(name: "sarah", email: "sarah@email.com", password: "12345678")
+  user_1 = User.create(name: "john", email: "john@email.com", password: "12345678")
+  user_2 = User.create(name: "martin", email: "martin@email.com", password: "12345678")
+  user_3 = User.create(name: "sarah", email: "sarah@email.com", password: "12345678")
 
-group_1 = Group.create(name: "gamezzz", owner: user_1)
-group_1.memberships.create(user: user_1)
+  group_1 = Group.create(name: "gamezzz", owner: user_1)
+  group_1.memberships.create(user: user_1)
+end
